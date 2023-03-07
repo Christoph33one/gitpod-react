7ee81a01cb05ecc5d734efae2ab31ec52056f7b5
Christoph33one <cjbrowning23@hotmail.com>
2023-03-07 12:14:59 +0000

    add statful component

--- my-app/src/components/StatfulGreeting.js
+++ my-app/src/components/StatfulGreeting.js
@@ -1,8 +1,24 @@
 import React from "react";
 
-// Added built in Component class
-class StatefulGreating extends React.Component {
+class StatefulGreeting extends React.Component{
+    // constructor is a built in function
+    constructor(props) {
+        super(props);
+        this.state = {
+            introduction: "Hello!",
+            buttonText: "Exit",
+        };
+    }
 
+
+    render() {
+        return (
+            <div>
+                <h1>{this.state.introduction} {this.props.greeting}</h1>
+                <button>{this.state.buttonText}</button>
+            </div>
+        )
+    }
 }
 
-export default StatefulGreating
+export default StatefulGreeting;
