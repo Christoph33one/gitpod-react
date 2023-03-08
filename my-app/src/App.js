c16430abceb7ae3e0539e60075b5faf0b158307b
Christoph33one <cjbrowning23@hotmail.com>
2023-03-08 17:28:29 +0000

    corrected redering erros

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import FunctionalGreating from './components/FunctionalGreating';
-import FunctionalGreatingWithProps from './components/FunctionalGreatingWithProps';
-import StatefulGreeting from './components/StatfulGreeting';
-// import StatefulGreeting from './components/StatefulGreetingWithCallBack';
+// import FunctionalGreating from './components/FunctionalGreating';
+// import FunctionalGreatingWithProps from './components/FunctionalGreatingWithProps';
+// import StatefulGreeting from './components/StatfulGreeting';
+// import StatefulGreetingWithCallback from "./components/StatefulGreetingWithCallback";
 
 function App() {
   return (
     <div className="App">
-      {/* <FunctionalGreating /> */}
+      {/* <FunctionalGreating />
       <StatefulGreeting  greeting="I'm a stateful class component!"/>
-      {/* <FunctionalGreatingWithProps greeting="nice to meet you!" name="Chris" age="34" /> */}
+      <FunctionalGreatingWithProps greeting="nice to meet you!" name="Chris" age="34" /> */}
+      {/* <StatefulGreetingWithCallback /> */}
     </div>
   );
 }
--- my-app/src/components/StatefulGreetingWithCallBack.js
+++ my-app/src/components/StatefulGreetingWithCallBack.js
@@ -1,39 +1,38 @@
-// import React from "react";
+import React from "react";
 
-// class StatefulGreeting extends React.Component{
-//     // constructor is a built in function
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             introduction: "Hello!",
-//             buttonText: "on",
+class StatefulGreetingWithCallback extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      introduction: "Hello!",
+      buttonText: "Exit",
+    };
+  }
 
-//         };
-//     }
-// // updating the state to give "good bye" message when button is clicked. Use setState!
-// // setState will render once the state is updated.
-// // same for button on and off once clicked!
-//     handleClick() {
-//         this.setState({
-//             introduction: "Goodbye!",
-//         });
-//         console.log(this.state.introduction);
-//         this.setState({
-//             buttonText: "off",
-//         });
-//         console.log(this.state.buttonOn);
-//     }
+  handleClick() {
+    this.setState({
+      introduction: "Goodbye!",
+      buttonText: 'Enter'
+    }, ()=>{
+        console.log('new state', this.state.introduction);
+        console.log('new state', this.state.buttonText);
+    });
+    console.log(this.state.introduction);
+    console.log(this.state.buttonText);
+  }
 
+  render() {
+    return (
+      <div>
+        <h1>
+          {this.state.introduction} {this.props.greeting}
+        </h1>
+        <button onClick={() => this.handleClick()}>
+          {this.state.buttonText}
+        </button>
+      </div>
+    );
+  }
+}
 
-
-//     render() {
-//         return (
-//             <div>
-//                 <h1>{this.state.introduction}{this.props.greeting}</h1>
-//                 <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
-//             </div>
-//         )
-//     }
-// }
-
-// export default StatefulGreeting;
+export default StatefulGreetingWithCallback;
